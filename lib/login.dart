0c00091b464ced2169311a976a2bbe10d6f57dc2
joshFeenberg <joshuafeenberg@gmail.com>
2024-09-04 14:02:44 -0500

    login page init

---
+++ lib/login.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Login',
+          style: TextStyle(color: Colors.blue),
+        ),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to the Login Screen!',
+          style: TextStyle(color: Colors.blue),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'login.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -14,16 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
   
     return MaterialApp(
-      title: 'Flutter Firebase Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Firebase Integration',
-          style: TextStyle(color: Colors.blue),),
-        ),
-        body: Center(
-          child: Text('Firebase is now integrated!'),
-        ),
-      ),
+      title: "Money Monkey",
+      home: LoginScreen(),
     );
   }
 }
