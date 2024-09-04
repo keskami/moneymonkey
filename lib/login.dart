32708996a470fedbee7357df054c4ee82d124e87
joshFeenberg <joshuafeenberg@gmail.com>
2024-09-04 15:19:05 -0500

    Container with create and Login

--- lib/login.dart
+++ lib/login.dart
@@ -1,19 +1,106 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  int _selectedIndex = 0;
+
+  void _onButtonPressed(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Login',
-          style: TextStyle(color: Colors.blue),
-        ),
-      ),
-      body: Center(
-        child: Text(
-          'Welcome to the Login Screen!',
-          style: TextStyle(color: Colors.blue),
+    return SafeArea(
+      child: Scaffold(
+        body: SizedBox(
+          width: double.infinity,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  height: 1,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.lightGreen,
+                  ),
+                ),
+                SizedBox(height: 18),
+                SizedBox(
+                  height: 744,
+                  width: double.infinity,
+                  child: Stack(
+                    alignment: Alignment.topCenter,
+                    children: [
+                      Image.asset(
+                        'assets/images/monkey.png',
+                        height: 390,
+                        width: double.infinity,
+                        alignment: Alignment.topCenter,
+                      ),
+                      Positioned(
+                        top: 195,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        child: Container(
+                          padding: EdgeInsets.all(30),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(
+                              color: Colors.grey.withOpacity(.5),
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  TextButton(
+                                    onPressed: () => _onButtonPressed(0),
+                                    child: Text("Create Account",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 22,
+
+                                    ),
+                                    ),
+                                  ),
+                                  TextButton(
+                                    onPressed: () => _onButtonPressed(1),
+                                    child: Text("Log in",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 22,
+                                    ),),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 20), // Add some spacing
+                              _selectedIndex == 0
+                                  ? Text('Create Account',
+                                      style: TextStyle(fontSize: 24))
+                                  : Text('Log in',
+                                      style: TextStyle(fontSize: 24)),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
