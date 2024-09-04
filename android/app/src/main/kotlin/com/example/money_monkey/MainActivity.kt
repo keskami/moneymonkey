7af5efc28fc02df45964e349e5509bb7f161d39f
joshFeenberg <joshuafeenberg@gmail.com>
2024-09-04 13:52:36 -0500

    fixed firebase name error

--- android/app/src/main/kotlin/com/example/money_monkey/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/money_monkey/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.money_monkey
+package com.moneymonkey.app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -13,11 +12,13 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+  
     return MaterialApp(
       title: 'Flutter Firebase Demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Firebase Integration'),
+          title: Text('Firebase Integration',
+          style: TextStyle(color: Colors.blue),),
         ),
         body: Center(
           child: Text('Firebase is now integrated!'),
