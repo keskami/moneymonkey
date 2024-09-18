b8d5d5cdbdfba3c3e58de5936e79fcdca44e7d19
joshFeenberg <joshuafeenberg@gmail.com>
2024-09-17 19:20:30 -0500

    fixed API issue

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'login.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
+    name: 'com.example.moneyMonkey',
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MyApp());
