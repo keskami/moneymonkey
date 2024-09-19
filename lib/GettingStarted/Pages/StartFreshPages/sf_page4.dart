a0b70ae27a115ccccbbe836cc04c94bd7e012d49
bhavyabedi <125470205+bhavyabedi@users.noreply.github.com>
2024-09-20 04:12:44 +0530

    4 pages

--- lib/GettingStarted/Pages/StartFreshPages/sf_page4.dart
+++ lib/GettingStarted/Pages/StartFreshPages/sf_page4.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:money_monkey/GettingStarted/Widgets/chat_bubble.dart';
 
 class StartFreshPage4 extends StatelessWidget {
@@ -42,6 +43,95 @@ class StartFreshPage4 extends StatelessWidget {
           ),
           const SizedBox(
             height: 20,
+          ), //First Row
+          Row(
+            mainAxisAlignment:
+                MainAxisAlignment.start, // Distribute space between widgets
+            children: [
+              Image.asset(
+                "assets/images/rework.png",
+                width: 100,
+              ),
+              RichText(
+                text: TextSpan(children: [
+                  TextSpan(
+                    text: "Master Financial Habits",
+                    style: GoogleFonts.fredoka().copyWith(
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(
+                    text:
+                        "\nBudgeting tools, savings plans,\nand expense tracking.",
+                    style: GoogleFonts.fredoka().copyWith(
+                      fontSize: 15,
+                      color: Colors.black,
+                    ),
+                  ),
+                ]),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment:
+                MainAxisAlignment.start, // Distribute space between widgets
+            children: [
+              Image.asset(
+                "assets/images/blue_icon.png",
+                width: 100,
+              ),
+              RichText(
+                text: TextSpan(children: [
+                  TextSpan(
+                    text: "Build your Financial Blueprint",
+                    style: GoogleFonts.fredoka().copyWith(
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(
+                    text:
+                        "\nFinancial planning guides, investment\nsimulations, and personalized advice.",
+                    style: GoogleFonts.fredoka().copyWith(
+                      fontSize: 15,
+                      color: Colors.black,
+                    ),
+                  ),
+                ]),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment:
+                MainAxisAlignment.start, // Distribute space between widgets
+            children: [
+              Image.asset(
+                "assets/images/watch.png",
+                width: 100,
+              ),
+              RichText(
+                text: TextSpan(children: [
+                  TextSpan(
+                    text: "Develop a Learning Habit",
+                    style: GoogleFonts.fredoka().copyWith(
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "\nSmart reminders, fun challenges,\nand more.",
+                    style: GoogleFonts.fredoka().copyWith(
+                      fontSize: 15,
+                      color: Colors.black,
+                    ),
+                  ),
+                ]),
+              )
+            ],
           ),
         ],
       ),
