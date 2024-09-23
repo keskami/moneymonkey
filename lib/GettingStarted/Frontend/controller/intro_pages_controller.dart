70777521f2553c9581c258ee20035346519e5bde
bhavyabedi <125470205+bhavyabedi@users.noreply.github.com>
2024-09-23 15:14:45 +0530

    Final Commit for PR

--- lib/GettingStarted/Frontend/controller/intro_pages_controller.dart
+++ lib/GettingStarted/Frontend/controller/intro_pages_controller.dart
@@ -13,7 +13,7 @@ class GettingStartedController extends GetxController {
     const GettingStartedPage2(),
     const GettingStartedPage3(),
     const GettingStartedPage4(),
-    GettingStartedPage5(),
+    const GettingStartedPage5(),
     const GettingStartedPage6(),
   ];
   RxInt age = 0.obs;
