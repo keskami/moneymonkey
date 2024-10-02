498cb0fe84b7d7086b04e07d06bb56e8c4859439
bhavyabedi <125470205+bhavyabedi@users.noreply.github.com>
2024-10-03 01:36:32 +0530

    Added Images to the Backend Storage. Implemented new changes to all needed pages. Started working on Profile Page as well.

---
+++ lib/Profile Page/profile_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: const Stack(
+      children: [],
+    ));
+  }
+}
