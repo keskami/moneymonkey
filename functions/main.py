23aa26dd1ba3b463cbf50fa293009a1cf4fb78cc
joshFeenberg <joshuafeenberg@gmail.com>
2024-11-05 07:59:38 -0600

    fixed reading everytime issue only reads once now. next step is figuring out how to combine so there are not too many in storage

--- functions/main.py
+++ functions/main.py
@@ -6,6 +6,9 @@ import time
 from statistics import mean
 import random
 from datetime import datetime, timedelta
+from collections import deque
+
+#make it so data is not reread using dequee 
 
 
 
@@ -28,6 +31,11 @@ def consolidate_data(stock_names):
             db.collection(stock).add(hourly_data)
             for doc in stock_ref:
                 db.collection(stock).document(doc.id).delete()'''
+
+BananaTechVals = deque(maxlen=100)
+HealthyChimpVals = deque(maxlen=100)
+EcoVineVals = deque(maxlen=100)
+JungleGoodsVals = deque(maxlen=100)
     
 
 def pickValue(values, stock):
@@ -44,43 +52,91 @@ def start():
     while True:
         with app.app_context():  
             on_request_example(None)  
-        time.sleep(10)  
+        time.sleep(5)  
 
 @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
+    global lastBanana, lastChimp, LastEconVine, LastJungle
     BananaTechValue = 124
     HealthyChimpValue = 64
     EcoVineValue = 80
     JungleGoodsValue = 55
+
     try:
-        BananaTechVals = []
-        HealthyChimpVals = []
-        EcoVineVals = []
-        JungleGoodsVals = []
+        if len(HealthyChimpVals) >= 1 and len(BananaTechVals) >= 1 and len(EcoVineVals) >= 1 and len(JungleGoodsVals) >= 1:
+            print("here")
+            BananaTechVals.append(lastBanana)
+            HealthyChimpVals.append(lastChimp)
+            EcoVineVals.append(LastEconVine)
+            JungleGoodsVals.append(LastJungle)
+            if BananaTechVals:
+                BananaTechValue = round(pickValue(BananaTechVals, 'BananaTech'), 2)
+            if HealthyChimpVals:
+                HealthyChimpValue = round(pickValue(HealthyChimpVals, 'HealthyChimp'), 2)
+            if EcoVineVals:
+                EcoVineValue = round(pickValue(EcoVineVals, 'EcoVine'), 2)
+            if JungleGoodsVals:
+                JungleGoodsValue = round(pickValue(JungleGoodsVals, 'JungleGoods'), 2)
+
+        else:
+            print("WARNING READING")
+            banana_ref = db.collection('BananaTech').order_by('timestamp', direction=firestore.Query.DESCENDING).limit(1).get()
+            chimp_ref = db.collection('HealthyChimp').order_by('timestamp', direction=firestore.Query.DESCENDING).limit(1).get()
+            ecovine_ref = db.collection('EcoVine').order_by('timestamp', direction=firestore.Query.DESCENDING).limit(1).get()
+            jungle_ref = db.collection('JungleGoods').order_by('timestamp', direction=firestore.Query.DESCENDING).limit(1).get()
+            
+            if banana_ref:
+                for doc in banana_ref:
+                    doc_data = doc.to_dict()
+                    value = doc_data.get('value')
+                    if value not in BananaTechVals:   
+                        BananaTechVals.append(value)
+                lastBanana = value
+            if chimp_ref:
+                for doc in chimp_ref:
+                    doc_data = doc.to_dict()
+                    value = doc_data.get('value')
+                    if value not in HealthyChimpVals:
+                        HealthyChimpVals.append(value)
+                lastChimp = value
+            if ecovine_ref:
+                for doc in ecovine_ref:
+                    doc_data = doc.to_dict()
+                    value = doc_data.get('value')
+                    if value not in EcoVineVals:
+                        EcoVineVals.append(value)
+                LastEconVine = value
+            if jungle_ref:
+                for doc in jungle_ref:
+                    doc_data = doc.to_dict()
+                    value = doc_data.get('value')
+                    if value not in JungleGoodsVals:
+                        JungleGoodsVals.append(doc_data.get('value'))
+                LastJungle = value
         
-        banana_ref = db.collection('BananaTech').order_by('timestamp', direction=firestore.Query.DESCENDING).limit(30).get()
-        chimp_ref = db.collection('HealthyChimp').order_by('timestamp', direction=firestore.Query.DESCENDING).limit(30).get()
-        ecovine_ref = db.collection('EcoVine').order_by('timestamp', direction=firestore.Query.DESCENDING).limit(30).get()
-        jungle_ref = db.collection('JungleGoods').order_by('timestamp', direction=firestore.Query.DESCENDING).limit(30).get()
-        
-        if banana_ref:
-            for doc in banana_ref:
-                doc_data = doc.to_dict()   
-                BananaTechVals.append(doc_data.get('value'))
-        if chimp_ref:
-            for doc in chimp_ref:
-                doc_data = doc.to_dict()
-                HealthyChimpVals.append(doc_data.get('value'))
-        if ecovine_ref:
-            for doc in ecovine_ref:
-                doc_data = doc.to_dict()
-                EcoVineVals.append(doc_data.get('value'))
-        if jungle_ref:
-            for doc in jungle_ref:
-                doc_data = doc.to_dict()
+            if BananaTechVals:
+                BananaTechValue = round(pickValue(BananaTechVals, 'BananaTech'), 2)
+            if HealthyChimpVals:
+                HealthyChimpValue = round(pickValue(HealthyChimpVals, 'HealthyChimp'), 2)
                 
-                JungleGoodsVals.append(doc_data.get('value'))
-        
+
+            if EcoVineVals:
+                EcoVineValue = round(pickValue(EcoVineVals, 'EcoVine'), 2)
+            if JungleGoodsVals:
+                JungleGoodsValue = round(pickValue(JungleGoodsVals, 'JungleGoods'), 2)
+    except Exception as e:
+        BananaTechValue = 124
+        BananaTechVals.append(BananaTechValue)
+        lastBanana = BananaTechValue
+        HealthyChimpValue = 64
+        HealthyChimpVals.append(HealthyChimpValue)
+        lastChimp = HealthyChimpValue
+        EcoVineValue = 80
+        EcoVineVals.append(EcoVineValue)
+        LastEconVine = EcoVineValue
+        JungleGoodsValue = 55
+        JungleGoodsVals.append(JungleGoodsValue)
+        LastJungle = JungleGoodsValue
         if BananaTechVals:
             BananaTechValue = round(pickValue(BananaTechVals, 'BananaTech'), 2)
         if HealthyChimpVals:
@@ -89,11 +145,6 @@ def on_request_example(req: https_fn.Request) -> https_fn.Response:
             EcoVineValue = round(pickValue(EcoVineVals, 'EcoVine'), 2)
         if JungleGoodsVals:
             JungleGoodsValue = round(pickValue(JungleGoodsVals, 'JungleGoods'), 2)
-    except Exception as e:
-        BananaTechValue = 124
-        HealthyChimpValue = 64
-        EcoVineValue = 80
-        JungleGoodsValue = 55
         print(e)
 
     BananaTech_data_to_add = {
@@ -120,9 +171,12 @@ def on_request_example(req: https_fn.Request) -> https_fn.Response:
         'value': HealthyChimpValue + BananaTechValue + EcoVineValue + JungleGoodsValue,
         'timestamp': firestore.SERVER_TIMESTAMP
     }
-    print(f"Attempting to add data: {BananaTech_data_to_add}, {HealthyChimp_data_to_add}, {EcoVine_data_to_add}, {JungleGoods_data_to_add}")
+    lastBanana = BananaTechValue
+    lastChimp = HealthyChimpValue
+    LastEconVine = EcoVineValue
+    LastJungle = JungleGoodsValue
+    print(f"Attempting to add data: {BananaTech_data_to_add}, {HealthyChimp_data_to_add}, {EcoVine_data_to_add}, {JungleGoods_data_to_add}, {stocksData}")
 
-  
     try:
         db.collection('BananaTech').add(BananaTech_data_to_add)
         db.collection('HealthyChimp').add(HealthyChimp_data_to_add)
