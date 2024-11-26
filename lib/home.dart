77084d535c65321bee5b0760fcbef30a6b79b8a7
bhavyabedi <125470205+bhavyabedi@users.noreply.github.com>
2024-11-26 21:01:22 +0530

    Temp

--- lib/home.dart
+++ lib/home.dart
@@ -16,8 +16,152 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   PageController _pageController = PageController();
   int currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return screenWidth > screenHeight
+        ? webDisplay(context, screenWidth)
+        : mobileDisplay(context);
+  }
+
+  Scaffold webDisplay(BuildContext context, double screenWidth) {
+    return Scaffold(
+      body: Row(
+        children: [
+          Container(
+            width: screenWidth * 0.15,
+            child: Column(
+              children: [
+                ListTile(
+                  onTap: () {
+                    setState(() {
+                      currentPage = 0;
+                    });
+                    _pageController.animateToPage(
+                      currentPage,
+                      duration: Duration(milliseconds: 200),
+                      curve: Curves.linear,
+                    );
+                  },
+                  selected: currentPage == 0,
+                  leading: Image.asset('assets/images/globemonkey.png'),
+                  trailing: Text(
+                    "Home",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 20,
+                  ),
+                ),
+                ListTile(
+                  onTap: () {
+                    setState(() {
+                      currentPage = 1;
+                    });
+                    _pageController.animateToPage(
+                      currentPage,
+                      duration: Duration(milliseconds: 200),
+                      curve: Curves.linear,
+                    );
+                  },
+                  selected: currentPage == 1,
+                  leading: Image.asset('assets/images/treasure.png'),
+                  trailing: Text(
+                    "Portfolio",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 20,
+                  ),
+                ),
+                ListTile(
+                  onTap: () {
+                    setState(() {
+                      currentPage = 2;
+                    });
+                    _pageController.animateToPage(
+                      currentPage,
+                      duration: Duration(milliseconds: 200),
+                      curve: Curves.linear,
+                    );
+                  },
+                  selected: currentPage == 2,
+                  leading: Image.asset('assets/images/bottommonkey.png'),
+                  trailing: Text(
+                    "Market",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 20,
+                  ),
+                ),
+                ListTile(
+                  onTap: () {
+                    setState(() {
+                      currentPage = 3;
+                    });
+                    _pageController.animateToPage(
+                      currentPage,
+                      duration: Duration(milliseconds: 200),
+                      curve: Curves.linear,
+                    );
+                  },
+                  selected: currentPage == 3,
+                  leading: Image.asset('assets/images/bluemonkey.png'),
+                  trailing: Text(
+                    "Profile",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: screenWidth * 0.85,
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (value) {
+                setState(() {
+                  currentPage = value;
+                });
+              },
+              children: [
+                HomeScreen(),
+                PortfolioScreen(),
+                ComingSoonPage(),
+                ProfileScreen(),
+              ],
+            ),
+          ),
+        ],
+      ),
+      // bottomNavigationBar: _buildMobileBottomBar(context),
+    );
+  }
+
+  Scaffold mobileDisplay(BuildContext context) {
     return Scaffold(
       body: PageView(
         controller: _pageController,
@@ -33,11 +177,11 @@ class _HomePageState extends State<HomePage> {
           ProfileScreen(),
         ],
       ),
-      bottomNavigationBar: _buildBottomBar(context),
+      bottomNavigationBar: _buildMobileBottomBar(context),
     );
   }
 
-  Widget _buildBottomBar(BuildContext context) {
+  Widget _buildMobileBottomBar(BuildContext context) {
     return BottomNavigationBar(
       currentIndex: currentPage,
       onTap: (index) {
@@ -47,7 +191,6 @@ class _HomePageState extends State<HomePage> {
           duration: Duration(milliseconds: 100),
           curve: Curves.linear,
         );
-        currentPage = index;
       },
       backgroundColor: Colors.white,
       type: BottomNavigationBarType.fixed,
@@ -56,15 +199,15 @@ class _HomePageState extends State<HomePage> {
       showSelectedLabels: false,
       showUnselectedLabels: false,
       items: [
-        _buildNavItem('assets/images/globemonkey.png', 0),
-        _buildNavItem('assets/images/treasure.png', 1),
-        _buildNavItem('assets/images/bottommonkey.png', 2),
-        _buildNavItem('assets/images/bluemonkey.png', 3),
+        _buildMobileNavItem('assets/images/globemonkey.png', 0),
+        _buildMobileNavItem('assets/images/treasure.png', 1),
+        _buildMobileNavItem('assets/images/bottommonkey.png', 2),
+        _buildMobileNavItem('assets/images/bluemonkey.png', 3),
       ],
     );
   }
 
-  BottomNavigationBarItem _buildNavItem(String iconPath, int index) {
+  BottomNavigationBarItem _buildMobileNavItem(String iconPath, int index) {
     final screenSize = MediaQuery.of(context).size;
     double iconSize = screenSize.width * 0.13;
 
