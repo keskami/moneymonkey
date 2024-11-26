6c037eb93b41e0c3d2501fa535f9490f4890d733
joshFeenberg <joshuafeenberg@gmail.com>
2024-11-26 11:02:10 -0600

    nav bar gone on web

--- MM-app/lib/home.dart
+++ MM-app/lib/home.dart
@@ -14,9 +14,14 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final HomeController homeController = Get.put(HomeController());
+  late double screenWidth;
+  late double screenHeight;
+
 
   @override
   Widget build(BuildContext context) {
+       screenWidth = MediaQuery.of(context).size.width;
+       screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Obx(
         () => homeController.pages[homeController.pageIndex.value],
@@ -25,8 +30,11 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+
+
   Widget _buildBottomBar(BuildContext context) {
-    return BottomNavigationBar(
+    return screenHeight > screenWidth ?
+    BottomNavigationBar(
       currentIndex: homeController.pageIndex.value,
       onTap: (index) {
         setState(() {
@@ -45,12 +53,59 @@ class _HomePageState extends State<HomePage> {
         _buildNavItem('assets/images/bottommonkey.png', 2),
         _buildNavItem('assets/images/bluemonkey.png', 3),
       ],
+    ) : 
+    
+    
+    BottomNavigationBar(
+      currentIndex: homeController.pageIndex.value,
+      onTap: (index) {
+        setState(() {
+          homeController.pageIndex.value = index;
+        });
+      },
+      backgroundColor: Colors.white,
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: Colors.blue,
+      unselectedItemColor: Colors.grey,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      items: [
+        _buildWebNavItem('assets/images/globemonkey.png', 0),
+        _buildWebNavItem('assets/images/treasure.png', 1),
+        _buildWebNavItem('assets/images/bottommonkey.png', 2),
+        _buildWebNavItem('assets/images/bluemonkey.png', 3),
+      ],
     );
   }
 
   BottomNavigationBarItem _buildNavItem(String iconPath, int index) {
     final screenSize = MediaQuery.of(context).size;
     double iconSize = screenSize.width * 0.13;
+
+    return BottomNavigationBarItem(
+      icon: Container(
+        width: iconSize,
+        height: iconSize,
+        decoration: BoxDecoration(
+          border: homeController.pageIndex.value == index
+              ? Border.all(
+                  color: Colors.blue, width: 3) // Border for the selected item
+              : null,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        padding: const EdgeInsets.all(8),
+        child: Image.asset(
+          iconPath,
+          fit: BoxFit.contain,
+        ),
+      ),
+      label: '', // No label
+    );
+  }
+
+  BottomNavigationBarItem _buildWebNavItem(String iconPath, int index) {
+    final screenSize = MediaQuery.of(context).size;
+    double iconSize = screenSize.width * 0;
 
     return BottomNavigationBarItem(
       icon: Container(
