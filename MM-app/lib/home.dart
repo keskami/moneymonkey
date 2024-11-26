3aa836aad2d6f29824253ee9f7da2d76e84e026b
joshFeenberg <joshuafeenberg@gmail.com>
2024-11-26 11:40:13 -0600

    web drawer good. Need to add names to other two pages

--- MM-app/lib/home.dart
+++ MM-app/lib/home.dart
@@ -33,7 +33,7 @@ class _HomePageState extends State<HomePage> {
               () => homeController.pages[homeController.pageIndex.value],
             ),
       drawer: isWideScreen ? null : _buildDrawer(), 
-      bottomNavigationBar: !isWideScreen ? _buildBottomBar() : null,
+      bottomNavigationBar: !isWideScreen ? _buildBottomBar(context) : null,
     );
   }
 
@@ -52,33 +52,28 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _buildDrawerContent() {
+    final screenHeight = MediaQuery.of(context).size.height;
     return ListView(
       padding: EdgeInsets.zero,
-      children: <Widget>[
-        DrawerHeader(
-          decoration: BoxDecoration(
-            color: Colors.blue,
-          ),
-          child: Text(
-            'Navigation',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-            ),
-          ),
-        ),
-        _buildDrawerItem('assets/images/globemonkey.png', 0),
-        _buildDrawerItem('assets/images/treasure.png', 1),
-        _buildDrawerItem('assets/images/bottommonkey.png', 2),
-        _buildDrawerItem('assets/images/bluemonkey.png', 3),
+      children: 
+      <Widget>[
+        SizedBox(height: screenHeight * .1,),
+        _buildDrawerItem('assets/images/globemonkey.png', 0,screenHeight, "Home"),
+        SizedBox(height: screenHeight * .05,),
+        _buildDrawerItem('assets/images/treasure.png', 1,screenHeight, "Porfolio"),
+        SizedBox(height: screenHeight * .05,),
+        _buildDrawerItem('assets/images/bottommonkey.png', 2, screenHeight, "IDK"),
+        SizedBox(height: screenHeight * .05,),
+        _buildDrawerItem('assets/images/bluemonkey.png', 3, screenHeight, "IDK"),
       ],
     );
   }
 
-  Widget _buildDrawerItem(String iconPath, int index) {
+  Widget _buildDrawerItem(String iconPath, int index, double screenHeight, String page) {
+    
     return ListTile(
-      leading: Image.asset(iconPath, width: 24),
-      title: Text('Page $index'),
+      leading: Image.asset(iconPath, height: screenHeight * .15),
+      title: Text('$page'),
       selected: homeController.pageIndex.value == index,
       onTap: () {
         setState(() {
@@ -88,7 +83,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _buildBottomBar() {
+  Widget _buildBottomBar(BuildContext context) {
     return BottomNavigationBar(
       currentIndex: homeController.pageIndex.value,
       onTap: (index) {
@@ -97,8 +92,11 @@ class _HomePageState extends State<HomePage> {
         });
       },
       backgroundColor: Colors.white,
+      type: BottomNavigationBarType.fixed,
       selectedItemColor: Colors.blue,
       unselectedItemColor: Colors.grey,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
       items: [
         _buildNavItem('assets/images/globemonkey.png', 0),
         _buildNavItem('assets/images/treasure.png', 1),
@@ -109,9 +107,27 @@ class _HomePageState extends State<HomePage> {
   }
 
   BottomNavigationBarItem _buildNavItem(String iconPath, int index) {
+    final screenSize = MediaQuery.of(context).size;
+    double iconSize = screenSize.width * 0.13;
+
     return BottomNavigationBarItem(
-      icon: Image.asset(iconPath, width: 24),
-      label: '',
+      icon: Container(
+        width: iconSize,
+        height: iconSize,
+        decoration: BoxDecoration(
+          border: homeController.pageIndex.value == index
+              ? Border.all(
+                  color: Colors.blue, width: 3) // Border for the selected item
+              : null,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        padding: const EdgeInsets.all(8),
+        child: Image.asset(
+          iconPath,
+          fit: BoxFit.contain,
+        ),
+      ),
+      label: '', // No label
     );
   }
-}
+  }
